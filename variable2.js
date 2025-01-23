02767f1bc66e1121c921d22a43e5c0aa5be90bd1
BernardoCastillo <bernardocastillo6921@gmail.com>
2025-01-23 12:13:30 -0700

    first commit

---
+++ variable2.js
@@ -0,0 +1,18 @@
+const name = "John Doe";
+const age = 28;
+const occupation = "Web Developer";
+const bio = "Passionate about coding and teaching others.";
+const profilePicture = "../profile-card-webapp/profile.png";
+const profileContainer = document.getElementById("profile-container");
+
+
+
+const profileCard = '<div class="card">' +
+'<img src="' + profilePicture + '" alt="Profile Picture">' +
+'<h2>' + name + '</h2>' +
+'<p>Age: ' + age + '</p>' +
+'<p>Occupation: ' + occupation + '</p>' +
+'<p>' + bio + '</p>' +
+'</div';
+
+profileContainer.innerHTML = profileCard;
